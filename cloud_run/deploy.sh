0a7ff241f0d0572473bc026b9f6d56234e1de136
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-06-14 13:35:47 +0900

    delete cloud run

--- cloud_run/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-gcloud builds submit --tag gcr.io/$gcp_project/morning-ai
-gcloud beta run deploy morning-ai --image gcr.io/$gcp_project/morning-ai --platform managed --region us-west1
