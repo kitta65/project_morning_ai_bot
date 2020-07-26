f43f6acb38db55aa00451d18cf9093b5d9f3a597
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-07-26 16:55:44 +0900

    premium search -> timeline, hashtag -> oldest

--- functions/main.py
+++ functions/main.py
@@ -4,36 +4,47 @@ import json
 import requests
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
+from pytz import utc, timezone
 
 #===== common =====
 class MyException(Exception):
     pass
 
-# https://developer.twitter.com/en/docs/tweets/search/api-reference/premium-search
-def search_tweets(token, env_label, query, from_jst=None, to_jst=None):
-    url = f"https://api.twitter.com/1.1/tweets/search/30day/{env_label}.json"
+# https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline
+def search_oldest_tweet(token, name, from_jst=None):
+    """
+    from_jst... yyyymmddhhmm
+    """
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
     time_diff = datetime.timedelta(hours=9)
     headers = {
         "Authorization": f"Bearer {token}"
     }
     payload = {
-        "query": query,
-        "maxResults": 100,
+        "count": 10,
+        "screen_name": name,
+        "trim_user": "t"
     }
     if from_jst is not None:
-        from_jst_parsed = datetime.datetime.strptime(from_jst, "%Y%m%d%H%M")
-        from_utc = (from_jst_parsed - time_diff).strftime("%Y%m%d%H%M")
+        from_datetime = datetime.datetime.strptime(from_jst+"+0900", "%Y%m%d%H%M%z")
     else:
-        from_utc = (datetime.datetime.now() - datetime.timedelta(hours=12)).strftime("%Y%m%d%H%M")
-    payload["fromDate"] = from_utc
-    if to_jst is not None:
-        to_jst_parsed = datetime.datetime.strptime(to_jst, "%Y%m%d%H%M")
-        to_utc = (to_jst_parsed - time_diff).strftime("%Y%m%d%H%M")
-        payload["toDate"] = to_utc
+        from_tmp = datetime.datetime.now(timezone("Asia/Tokyo"))
+        from_datetime = timezone("Asia/Tokyo").localize(datetime.datetime(
+            year=from_tmp.year,
+            month=from_tmp.month,
+            day=from_tmp.day,
+            hour=6,
+            minute=55
+        ))
     res = requests.get(url, headers=headers, params=payload)
     res_json = json.loads(res.text)
+    res_limitted = [
+        x
+        for x in res_json
+        if from_datetime <= datetime.datetime.strptime(x["created_at"], "%a %b %d %H:%M:%S %z %Y")
+    ]
     try:
-        tweet_id = res_json["results"][0]["id_str"]
+        tweet_id = res_limitted[-1]["id_str"]
         # results is automatically sorted in reverse chronological order
         tweet_url = f"https://twitter.com/aichan_nel/status/{tweet_id}"
     except (IndexError, KeyError) as e:
@@ -45,12 +56,10 @@ def send_message(message):
     line_bot_api.broadcast(TextSendMessage(text=message))
 
 def morning_ai_bot(request):
-    tweet_url = search_tweets(
+    tweet_url = search_oldest_tweet(
         config.TWITTER_TOKEN,
-        "myenv",
-        '#モーニングアイちゃん from:aichan_nel has:videos',
-        #"202006140600",
-        #"202006140900",
+        'aichan_nel',
+        #"202007261259",
     )
     send_message(tweet_url)
 
