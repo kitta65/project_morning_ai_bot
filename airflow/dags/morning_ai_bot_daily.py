a855e0f563daf118a0a3e8f16d8bb4e267e1bea3
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-10-21 00:44:02 +0900

    routine

--- airflow/dags/morning_ai_bot_daily.py
+++ airflow/dags/morning_ai_bot_daily.py
@@ -4,7 +4,7 @@ from morning_ai_bot_package.morning_ai_bot_config import *
 
 
 dag = DAG(
-    'morning_ai_bot_daily_v0.3',
+    'morning_ai_bot_daily',
     default_args=common_args,
     description='call funtion mornin_ai_bot',
     schedule_interval="05 22 *  *  *",
@@ -15,8 +15,8 @@ task1 = PythonOperator(
     python_callable=exec_functions,
     provide_context=True,
     op_kwargs={
-        "url": "https://us-central1-{}.cloudfunctions.net/morning_ai_bot".format(gcp_project),
-        "token": line_token
+        "url": "https://us-central1-{}.cloudfunctions.net/morning_ai_bot".format(GCP_PROJECT),
+        "token": LINE_TOKEN
     },
     dag=dag,
 )
